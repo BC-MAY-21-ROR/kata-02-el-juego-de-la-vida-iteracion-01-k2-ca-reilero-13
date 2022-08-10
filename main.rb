479fd07dba68c30ff7c6b91f2a27e99b52dd5081
Davinson Castañeda <davinsonbmx@gmail.com>
2022-08-09 19:30:59 -0500

    Renamed variables and refactored code

--- main.rb
+++ main.rb
@@ -1,147 +1,85 @@
-# input = gets.chomp.split(' ')
-# rows = input[0].to_i
-# cols = input[1].to_i
-
-# grid = []
-# rows.times do
-#   grid.append(gets)
-# end
- 
-grid = [
-  '.*......',
-  '*****...',
-  '..***...',
-  '..***...' 
-]
-
-# puts grid[0]
-# puts grid[0][0]
-# puts grid[0][1]
-
-def contar_vecinos(grid, fila, columna)
-  vecinos = 0
-  for k in -1..1 
-    # Contador de vecinos celdas de la fila arriba 
-    if fila - 1 >= 0 and columna + k >= 0 and columna + k < grid[0].length
-      if grid[fila - 1][columna + k] == '*'
-        vecinos += 1
-        # print(fila - 1, ' ', columna + k)
-        # puts ' '
-      end
-    end
-
-    # Contador de vecinos celdas de la fila de en medio
-    if fila >= 0 and columna + k >= 0 and columna + k < grid[0].length
-      if columna + k != columna
-        if grid[fila][columna + k] == '*'
-          vecinos += 1
-          # print(fila, ' ', columna + k)
-          # puts ' '
-        end
-      end
-    end
-    
-    
-    # Contador de vecinos celdas de la fila de abajo
-    if fila + 1 < grid.length and columna + k >= 0 and columna + k < grid[0].length
-      if grid[fila + 1][columna + k] == '*'
-        vecinos += 1
-        # print(fila + 1, ' ', columna + k)
-        # puts ''
-      end
-    end
-  end
-  return vecinos
-end
-
-# .*......
-# **..*...
-# ...**...
-# ........
-
-# for i in 0..grid.length - 1
-#   for j in 0..grid[i].length - 1
-#     puts grid[i][j]
-#   end
-# end
-
-# contar_vecinos(grid, 1, 0)
-# contar_vecinos(grid, 1, 1)
-puts "checando 2, 3"
-contar_vecinos(grid, 2, 3)
-puts "checando 1, 3"
-contar_vecinos(grid, 1, 3)
-puts "checando 1, 5"
-contar_vecinos(grid, 1, 5)
-
+# Game rules
 # Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
 # Any live cell with more than three live neighbours dies, as if by overcrowding.
 # Any live cell with two or three live neighbours lives on to the next generation.
 # Any dead cell with exactly three live neighbours becomes a live cell.
 
-def reglas_del_juego(numero_de_vecinos, celda)
+def game_rules(neighbors, cell)
 
-  resultado = ' '
+  result = '.'
 
-
-  if celda == '*'
-    if numero_de_vecinos < 2
-      resultado = '.'
-    elsif numero_de_vecinos > 3
-      resultado = "."
+  if cell == '*'
+    if neighbors < 2
+      result = '.'
+    elsif neighbors > 3
+      result = "."
     else 
-      resultado = '*'  
+      result = '*'  
     end 
   end
-  if celda == '.' and numero_de_vecinos == 3
-    resultado = '*'   
+  if cell == '.' and neighbors == 3
+    result = '*'   
   end
-  return resultado
+  return result
 end
 
+def count_neighbors(grid, row, col)
+  neighbors = 0
+  
+  for k in -1..1 
+    # Contador de vecinos cells de la row arriba 
+    if row - 1 >= 0 and col + k >= 0 and col + k < grid[0].length
+      if grid[row - 1][col + k] == '*'
+        neighbors += 1
+      end
+    end
+
+    # Contador de vecinos cells de la row de en medio
+    if row >= 0 and col + k >= 0 and col + k < grid[0].length
+      if col + k != col
+        if grid[row][col + k] == '*'
+          neighbors += 1
+        end
+      end
+    end
+
+    # Contador de vecinos cells de la row de abajo
+    if row + 1 < grid.length and col + k >= 0 and col + k < grid[0].length
+      if grid[row + 1][col + k] == '*'
+        neighbors += 1
+      end
+    end
+  end
+  return neighbors
+end
 
 def main ()
-  grid = [
-  '.*......',
-  '*****...',
-  '..***...',
-  '..***...' 
-]
-grid_temporal = [
-  '.*......',
-  '*****...',
-  '..***...',
-  '..***...' 
-]
+  puts 'Enter number of rows and columns '
+  puts ''
+  input = gets.chomp.split(' ')
+  rows = input[0].to_i
+  cols = input[1].to_i
 
+  grid = []
+
+  puts 'Enter Grid '
+  puts ''
+  rows.times do
+    grid.append(gets)
+  end
+
+  grid_aux = Array.new(rows) { '.' * cols }
 
   for i in 0..grid.length - 1
     for j in 0..grid[i].length - 1
-      vecinos = contar_vecinos(grid, i, j) 
-      grid_temporal[i][j] = reglas_del_juego(vecinos, grid[i][j])
-      #puts grid[i][j]
+      neighbors = count_neighbors(grid, i, j) 
+      grid_aux[i][j] = game_rules(neighbors, grid[i][j])
     end
   end
-  puts grid_temporal
+  
+  puts ' New generation'
+  puts ''
+  puts grid_aux
 end
 
 main()
-
-
-# Generacion 1
-# .*......
-# **..*...
-# ...**...
-# ........
-
-# Generacion 2
-# **......
-# **.**...
-# ...**...
-# ........
-
-# Generacion 3
-# ***.....
-# **.**...
-# ..***...
-# ........
